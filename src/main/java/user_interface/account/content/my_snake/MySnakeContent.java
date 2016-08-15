2ae20cc76a5a873231ceeaf09c275f303fa11002
Ann Chernivchan <annachernivchan@gmail.com>
2016-08-15 20:31:05 +0300

    optimization MySnake

--- src/main/java/user_interface/account/content/my_snake/MySnakeContent.java
+++ src/main/java/user_interface/account/content/my_snake/MySnakeContent.java
@@ -4,15 +4,14 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import main.Main;
 import main.WindowSettings;
 import client_server.SnakePlayer;
+import user_interface.ComponentBuilder;
 
 import static user_interface.account.MainMenu.CONTENT_WIDTH;
 
@@ -25,7 +24,7 @@ public class MySnakeContent extends HBox {
     private double red = snake.getColor().getRed() * 255;
     private double green = snake.getColor().getGreen() * 255;
     private double blue = snake.getColor().getBlue() * 255;
-    private final double WIDTH = CONTENT_WIDTH * 0.75;
+    private final double WIDTH = CONTENT_WIDTH * 0.6;
     private final double HEIGHT = WindowSettings.height / 2;
 
     {
@@ -34,7 +33,6 @@ public class MySnakeContent extends HBox {
 
         ImageView avatar = new ImageView();
         avatar.setImage(Main.user.getSnake().getAvatar());
-//        avatar.setStyle("-fx-border-color: white; -fx-border-width: 2px; -fx-background-color: deeppink");
         avatar.setFitHeight(IMAGE_SIZE);
         avatar.setFitWidth(IMAGE_SIZE);
 
@@ -46,39 +44,27 @@ public class MySnakeContent extends HBox {
         snakeAvatar.setAlignment(Pos.CENTER);
         snakeAvatar.setStyle("-fx-border-color: rgb(62, 82, 37); -fx-border-width: 2px; " +
                 "-fx-background-color: rgb(" + red + ", " + green + ", " + blue + ");");
-//        snake.getColor()
         snakeAvatar.getChildren().addAll(avatar, changeAvatar);
 
         Label lblName = getLabel("Ім'я: ");
         Label lblRating = getLabel("Рейтинг: ");
         Label lblColor = getLabel("Колір: ");
         Label lblAbout = getLabel("Паспорт: ");
-
         TextField name = getTextField(snake.getName());
         Label rating = getLabel(String.valueOf(snake.getRating()));
-        ColorPicker color = new ColorPicker();
-        color.setStyle("-fx-font-size: 15");
-        color.setValue(snake.getColor());
-        TextArea passport = new TextArea(snake.getAbout());
-        passport.setStyle("-fx-font-size: 15");
-        passport.setMaxHeight(70);
-
-        Button save = new Button("Зберегти");
-        save.setFont(new Font(15));
-        HBox box = new HBox();
-        box.setAlignment(Pos.BOTTOM_RIGHT);
-        box.getChildren().add(save);
+        ColorPicker color = getColorPicker(snake.getColor());
+        TextArea passport = getTextArea(snake.getAbout());
+        HBox box = getHBox("Зберегти");
 
         snakeInformation.addRow(0, lblName, name);
-        snakeInformation.addRow(1, lblRating, rating);
-        snakeInformation.addRow(2, lblColor, color);
-        snakeInformation.addRow(3, lblAbout, passport);
+        snakeInformation.addRow(1, lblColor, color);
+        snakeInformation.addRow(2, lblAbout, passport);
+        snakeInformation.addRow(3, lblRating, rating);
         snakeInformation.add(box, 1, 4);
-//        snakeInformation.setHgap(50);
+
         snakeInformation.setVgap(20);
         snakeInformation.setAlignment(Pos.CENTER);
         snakeInformation.setPadding(new Insets(25, 25, 25, 25));
-//        snakeInformation.setGridLinesVisible(true);
         snakeInformation.getColumnConstraints().add(new ColumnConstraints(100));
         snakeInformation.setStyle("-fx-background-color: rgb(159, 188, 94)");
 
@@ -87,6 +73,30 @@ public class MySnakeContent extends HBox {
     }
 
     public MySnakeContent() {}
+
+
+    private HBox getHBox(String text) {
+        HBox box = new HBox();
+        Button save = new Button(text);
+        save.setFont(new Font(15));
+        save.setStyle("-fx-background-color: rgb(92, 136, 59); -fx-border-color: rgb(62, 82, 37)");
+        box.setAlignment(Pos.BOTTOM_RIGHT);
+        box.getChildren().add(save);
+        return box;
+    }
+
+    private TextArea getTextArea(String about) {
+        TextArea passport = new TextArea(snake.getAbout());
+        passport.setMaxHeight(70);
+        return passport;
+    }
+
+    private ColorPicker getColorPicker(Color snakeColor) {
+        ColorPicker color = new ColorPicker();
+        color.setStyle("-fx-font-size: 15; -fx-border-color: rgb(62, 82, 37); -fx-background-color: rgb(201, 231, 133)");
+        color.setValue(snakeColor);
+        return color;
+    }
 
     private Label getLabel(String text) {
         Label label = new Label(text);
@@ -97,11 +107,11 @@ public class MySnakeContent extends HBox {
 
     private TextField getTextField(String info) {
         TextField textField = new TextField(info);
-        textField.setStyle("-fx-font-size: 15; -fx-background-color: rgb(62, 82, 37) , rgb(159, 188, 94) , transparent;\n" +
-                "    -fx-background-insets: 0 -1 -1 -1, 0 0 0 0, 0 -1 3 -1;");
+        textField.setStyle("-fx-font-size: 15; -fx-background-color: rgb(201, 231, 133); -fx-border-color: rgb(62, 82, 37)");
         textField.setMaxWidth(300);
-
         return textField;
     }
+
+
 
 }
